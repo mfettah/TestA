fa7c0febf2d26c482a5bd33ed3414a863eeae3de
AD-ITS\fettah <mfettah>
2021-07-26 12:18:26 +0200

    merge

--- src/App.java
+++ src/App.java
@@ -7,6 +7,14 @@ class App {
 
 	}
 
+	public int add(int x,int y) {
+		return x+y;
+	}
+	
+	public int sub(int x,int y) {
+		return x-y;
+	}
+	
 	public int mult(int x, int y) {
 		return x * y;
 	}
